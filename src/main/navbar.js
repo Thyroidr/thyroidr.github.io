dead40ba110cd67f858296be41ccf6def98cb22c
Ryan Wang <u6937003@anu.edu.au>
2022-02-15 21:04:44 +1100

    Made popover panel animated

--- src/main/navbar.js
+++ src/main/navbar.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import pfp from '../resource/pfp.jpg';
 import {
   Link
 } from 'react-router-dom';
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import { usePopper } from 'react-popper';
 import { useState } from 'react';
 
@@ -19,7 +18,18 @@ const pages = [
 function NavBar() {
   let [referenceElement, setReferenceElement] = useState();
   let [popperElement, setPopperElement] = useState();
-  let { styles, attributes } = usePopper(referenceElement, popperElement);
+  let { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'bottom',
+    strategy: 'fixed',
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [-35, 5],
+        }
+      }
+    ],
+  });
 
   return (
     <div className="bg-gray-200 py-4 flex h-14 drop-shadow-lg items-center width-screen fixed top-0 left-0 right-0 z-50">
@@ -36,34 +46,46 @@ function NavBar() {
           </Link>
         ))}
       </div>
-      <Popover className="absolute md:hidden scale-100 right-0 px-2 py-2 mx-2 my-2">
+      <Popover className="absolute md:scale-0 scale-100 right-0 px-2 py-2 mx-2 my-2">
         {({ open }) => (
           <>
-            <Popover.Button ref={setReferenceElement}>
+            <Popover.Button 
+              ref={setReferenceElement}
+              className="px-2 py-2"
+            >
               <FontAwesomeIcon 
                 className={`${open ? 'transform rotate-90' : ''}`}
                 icon={faBars}
               />
             </Popover.Button>
 
-            <Popover.Panel
-              ref={setPopperElement}
-              style={styles.popper}
-              {...attributes.popper}
-              className='bg-gray-100 py-1 drop-shadow-lg rounded-lg'
+            <Transition
+              enter="transition duration-100 ease-out"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
             >
-              <div className='grid'>
-                {pages.map((page) => (
-                  <Popover.Button as={Link}
-                    key={'page-' + page.name}
-                    to={page.link}
-                    className='text-slate-700 px-8 py-1'
-                  >
-                    {page.name}
-                  </Popover.Button>
-                ))}
-              </div>
-            </Popover.Panel>
+              <Popover.Panel
+                ref={setPopperElement}
+                style={styles.popper}
+                {...attributes.popper}
+                className='bg-gray-100 py-1 drop-shadow-lg rounded-lg'
+              >
+                <div className='grid'>
+                  {pages.map((page) => (
+                    <Popover.Button as={Link}
+                      key={'page-' + page.name}
+                      to={page.link}
+                      className='text-slate-700 px-8 py-1'
+                    >
+                      {page.name}
+                    </Popover.Button>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
           </>
         )}
       </Popover>
